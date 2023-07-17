1f1a4882c7d2b5216fd3a0481a4597c97f2aaef5
Thusatharan <thusatharan27@gmail.com>
2023-07-17 22:42:33 +0530

    added layout for routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,29 @@ import {
   RouterProvider,
   Route,
   Link,
+  Outlet,
 } from "react-router-dom";
 import Users from "./pages/users/users";
 import Products from "./pages/products/products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
 
 function App() {
 
   const Layout = () => {
     return (
       <div className="main">
-        
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
       </div>
     )
   }
@@ -21,15 +34,21 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>
-    },
-    {
-      path: "users",
-      element: <Users/>,
-    },
-    {
-      path: "products",
-      element: <Products/>,
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/products',
+          element: <Products />
+        },
+        {
+          path: '/users',
+          element: <Users/>
+        }
+      ]
     },
   ]);
 
